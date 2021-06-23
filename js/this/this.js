d7717852c202850b40d408cda1a8ed618b5bc872
KarenZohrabyan <zohrabyankaren96@gmail.com>
2021-06-24 02:45:01 +0400

    working with context 'this'

--- js/this/this.js
+++ js/this/this.js
@@ -12,3 +12,27 @@ const object = {
 
 console.log(object.info()) // Karen: 25
 console.log(object.infoArrowfunc()) // undefined: undefined
+
+
+const obj_2 = {
+    age: 25,
+    info: function () {
+        setTimeout(function () {
+            console.log(this.age)
+        }, 1000)
+    }
+}
+
+// obj_2.info()
+
+const name = 2
+function foo() {
+    const name = 2030203
+    return (function () {
+        return (() => {
+            return this.name
+        })()
+    })()
+}
+
+console.log(foo());
